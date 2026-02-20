e6184a8cec2619272c55bea09091dcf8a807e9ed
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2026-02-20 17:26:13 +0200

    add experiment to preditions

--- julia/5-plot-predictions.jl
+++ julia/5-plot-predictions.jl
@@ -37,6 +37,7 @@ prediction_bands_df_2 = CSV.read("output/prediction-bands-2.csv", DataFrame)
 prediction_bands_df_2.lower_bound = ifelse.(prediction_bands_df_2.lower_status .== "SCAN_BOUND_REACHED", 1e-4, prediction_bands_df_2.lower_bound)
 prediction_bands_df_2.upper_bound = ifelse.(prediction_bands_df_2.upper_status .== "SCAN_BOUND_REACHED", 40., prediction_bands_df_2.upper_bound)
 
+col = palette(:auto) |> collect
 fig2d = plot(
     xlim = (-5., 125.),
     ylim = (-1., 21.),
@@ -55,5 +56,15 @@ for subdf in groupby(prediction_bands_df_2, :output)
         linewidth = 1
     )
 end
+
+# add experimental points
+experimental_df = CSV.read("data/data-synthetic-known-sigma.csv", DataFrame)
+for subdf in groupby(experimental_df, "prob.mean")
+    scatter!(fig2d, subdf.t, subdf.measurement;
+        color = ifelse.(subdf[!, "prob.mean"] .== "drug_c", col[1], col[2]),
+        markersize = 6
+    )
+end
+
 fig2d
 savefig(fig2d, "output/2D-predicted.png")
